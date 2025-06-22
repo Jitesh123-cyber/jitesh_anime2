6bcfc552448df618e4ed7c5ae37a1f93da1aa685
Jitesh123-cyber <jiteshkumarviwhal@gmail.com>
2025-06-22 09:31:27 +0530

    Add files via upload

---
+++ script3.js
@@ -0,0 +1,28 @@
+gsap.from( ".image1",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+    y:60
+})
+
+gsap.from( ".image2",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+    x:60
+})
+
+gsap.from( ".image3",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+    y:-60
+})
+
+gsap.from( ".h1",{
+    delay: 0.4,
+    opacity: 0,
+    duration: 1,
+    y:-70
+})
+
